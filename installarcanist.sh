466b4386c458ad389d7b4e455c925881768af606
Ari Pringle <apringle@gmail.com>
2013-02-25 11:23:13 -0700

    changing default installation directory

--- installarcanist.sh
+++ installarcanist.sh
@@ -3,55 +3,63 @@ set -e
 
 # Downloads arcanist, libphutil, etc and configures your system
 
-LOC_DIR="/usr/local"
-BIN_DIR="$LOC_DIR/bin"
-PHP_DIR="$LOC_DIR/zend/include/php"
+if [ -z "$ARC_LOC_DIR" ]; then
+    ARC_LOC_DIR="/usr"
+fi
 
-if [ ! -w "$LOC_DIR" ]; then
+if [ -z "$ARC_BIN_DIR" ]; then
+    ARC_BIN_DIR="$ARC_LOC_DIR/bin"
+fi
+
+if [ -z "$ARC_ARC_PHP_DIR" ]; then
+    ARC_PHP_DIR="$ARC_LOC_DIR/share/php"
+fi
+
+if [ ! -w "$ARC_LOC_DIR" ]; then
     if [ -z "$SUDO_USER" ]; then
-        echo "Re-running installation with sudo (no permission on $LOC_DIR for current user)."
+        echo "Re-running installation with sudo (no permission on $ARC_LOC_DIR for current user)."
         exec sudo /bin/sh $0 $*
     else
-        echo "We can't seem to access ${LOC_DIR}. Please check permissions on this folder and try again."
+        echo "We can't seem to access ${ARC_LOC_DIR}. Please check permissions on this folder and try again."
         exit -1
     fi;
 fi;
 
-if [ ! -e "$PHP_DIR" ]; then
-    mkdir -p $PHP_DIR
+if [ ! -e "$ARC_PHP_DIR" ]; then
+    mkdir -p $ARC_PHP_DIR
 fi;
 
 # Install or update libphutil
 echo "Updating libphutil.."
-if [ -e "$PHP_DIR/libphutil" ]; then
-    $BIN_DIR/arc upgrade
+if [ -e "$ARC_PHP_DIR/libphutil" ]; then
+    $ARC_BIN_DIR/arc upgrade
 else
-    git clone git://github.com/facebook/libphutil.git "$PHP_DIR/libphutil"
-    git clone git://github.com/facebook/arcanist.git "$PHP_DIR/arcanist"
-    #git clone git://github.com/facebook/phabricator.git "$PHP_DIR/phabricator"
+    git clone git://github.com/facebook/libphutil.git "$ARC_PHP_DIR/libphutil"
+    git clone git://github.com/facebook/arcanist.git "$ARC_PHP_DIR/arcanist"
+    #git clone git://github.com/facebook/phabricator.git "$ARC_PHP_DIR/phabricator"
 fi
 
 # Install or update libavt
 echo "Updating libavt.."
-if [ -e "$PHP_DIR/libavt" ]; then
-    cd "$PHP_DIR/libavt" && git pull origin master
+if [ -e "$ARC_PHP_DIR/libavt" ]; then
+    cd "$ARC_PHP_DIR/libavt" && git pull origin master
 else
-    git clone git://github.com/diablomedia/avt-arcanist.git "$PHP_DIR/libavt"
+    git clone git://github.com/diablomedia/avt-arcanist.git "$ARC_PHP_DIR/libavt"
 fi
 
 # Register arc commands
 echo "Registering arc commands.."
 
 ## create-arcconfig
-ln -fs "$PHP_DIR/libavt/bin/create-arcconfig" "$BIN_DIR/create-arcconfig"
-chmod +x "$BIN_DIR/create-arcconfig"
+ln -fs "$ARC_PHP_DIR/libavt/bin/create-arcconfig" "$ARC_BIN_DIR/create-arcconfig"
+chmod +x "$ARC_BIN_DIR/create-arcconfig"
 
 ## update-arcanist
-ln -fs "$PHP_DIR/libavt/bin/update-arcanist" "$BIN_DIR/update-arcanist"
-chmod +x "$BIN_DIR/update-arcanist"
+ln -fs "$ARC_PHP_DIR/libavt/bin/update-arcanist" "$ARC_BIN_DIR/update-arcanist"
+chmod +x "$ARC_BIN_DIR/update-arcanist"
 
 ## arc
-echo "php $PHP_DIR/arcanist/scripts/arcanist.php  --load-phutil-library='$PHP_DIR/libavt/src' \"\$@\"" > "$BIN_DIR/arc"
-chmod +x "$BIN_DIR/arc"
+echo "php $ARC_PHP_DIR/arcanist/scripts/arcanist.php  --load-phutil-library='$ARC_PHP_DIR/libavt/src' \"\$@\"" > "$ARC_BIN_DIR/arc"
+chmod +x "$ARC_BIN_DIR/arc"
 
 echo "Done!"
